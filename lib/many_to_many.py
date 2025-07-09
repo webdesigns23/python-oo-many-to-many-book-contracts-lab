14f824aa96c99ad4fa1765824f909d189d8e2ae6
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-09 16:36:31 -0600

    set up intermediary classes for author, book, and contract

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,20 @@
 class Author:
-    pass
-
+    all =[]
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
 
 class Book:
-    pass
-
+    all =[]
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
 
 class Contract:
-    pass
+    all =[]
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
